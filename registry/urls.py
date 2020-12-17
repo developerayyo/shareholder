6809b5447cb1f5452e8da6c9dfa40230818882bd
Restyled.io <commits@restyled.io>
2020-12-17 00:41:20 +0000

    Restyled by isort

--- registry/urls.py
+++ registry/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include, re_path
 from django.contrib.auth.decorators import login_required
+from django.urls import include, path, re_path
+
 from . import views
 
 urlpatterns = [
